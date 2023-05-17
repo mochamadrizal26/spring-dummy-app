7893542090739fd6b84c31fa8045e246aad3871a
Mochamad Rizal Hidayat <rizalwednesday@gmail.com>
2023-05-17 16:16:14 +0200

    First commit

---
+++ src/test/java/com/example/springdummyapp/SpringDummyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdummyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDummyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
